5ef68588500a01c0e8fddd12f3bd9341c7d3190c
i3po <i3poac@gmail.com>
2024-03-18 17:40:12 +0300

    prod ready

--- lib/data/network/constants/endpoints.dart
+++ lib/data/network/constants/endpoints.dart
@@ -2,7 +2,7 @@ class Endpoints {
   Endpoints._();
 
   // base url
-  static const String baseUrl = "http://10.0.2.2:3000/v1";
+  static const String baseUrl = "https://i3poac.freemyip.com/api/v1";
 
   // receiveTimeout
   static const int receiveTimeout = 20000;
